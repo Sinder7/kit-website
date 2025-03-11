198a11325ef8d0497dcde4193509125ca16dad9a
rasul <rasul_sadullayev@mail.ru>
2025-03-11 08:43:31 +0300

    создание приложения main, создания таблицы Page

---
+++ kit/main/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.JSONField(default=list, blank=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="subpages"
+    )
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ["title"]
